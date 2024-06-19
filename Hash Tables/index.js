85b77cf2395c1aacac159d4e5d7168573e174af9
Krishna Gupta <121439479+krishnagptamcs@users.noreply.github.com>
2024-06-19 23:51:46 +0530

    Hash-table logic modified to avoid collision storing the key value as array inside array

--- Hash Tables/index.js
+++ Hash Tables/index.js
@@ -28,7 +28,34 @@ class HashTable {
     // converting the string in the numeric charcater
     const index = this.hash(key);
     //after obtainig the index storing its value
-    this.table[index] = value;
+    // this logic has a glitch , if the same length key is passed like name , mane will be passed then numeric digit for this will be same
+    // that means this is an anagram scenario , and thus value would be replaced , by new value , but we dont want that
+    // this.table[index] = value;
+
+    //SO TAKING DIFFRENT APPROACH
+    //taking the key value pair at a index
+    let bucket = this.table[index];
+
+    if (!bucket) {
+      //if bucket is null , that means no key-value is present at a given index
+      // add the given key,value in an array
+      bucket = [[key, value]];
+    } else {
+      // if there is already key value pair is present in the index
+      // checking the exsistance key , with the incoming key
+
+      const sameKeyItem = bucket.find((item) => item[0] === key);
+
+      if (sameKeyItem) {
+        // if key is same , ex esxisting kye "name" && incoming key "name" , then replace the value of that key
+        // item[0]  , is the key name at this position
+        //sameKeyItem[1]  , is showing the value at the 1st postion
+        sameKeyItem[1] = value;
+      } else {
+        // if the key is dffrent , then push the incomign the key value pair in the exsitig aaray
+        bucket.push([key, value]);
+      }
+    }
   }
 
   //Get method
@@ -37,14 +64,38 @@ class HashTable {
     const index = this.hash(key);
     // now converting into numeric digit by hash method
     // returng the value of that index
-    return this.table[index];
+    // return this.table[index];
+
+    //MODIFIED LOGIC
+    const bucket = this.table[index];
+
+    if (bucket) {
+      const sameKeyItem = bucket.find((item) => item[0] === key);
+
+      if (sameKeyItem) {
+        return sameKeyItem[1];
+      }
+    } else {
+      console.log("the given not exsist");
+    }
   }
 
   //Remove method
   //delete the value at a given key
   remove(key) {
     const index = this.hash(key);
-    this.table[index] = undefined;
+    // this.table[index] = undefined;
+
+    //MODIFIED LOGIC
+    const bucket = this.table[index];
+
+    if (bucket) {
+      const sameKeyItem = bucket.find((item) => item[0] === key);
+
+      if (sameKeyItem) {
+        bucket.splice(bucket.indexOf(sameKeyItem), 1);
+      }
+    }
   }
 
   //Display method
